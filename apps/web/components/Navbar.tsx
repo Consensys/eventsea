5a1ecd8c0f6f3af6e33692e471efade39fc7ecae
meowyx <qafka256@gmail.com>
2023-10-08 02:59:04 +0600

    implemented metamask sdk

--- apps/web/components/Navbar.tsx
+++ apps/web/components/Navbar.tsx
@@ -1,16 +1,66 @@
 "use client";
 
+// Navbar.tsx
+
 import Link from "next/link";
 import EventSeaLogo from "../public/icons/EventSeaLogo";
 import WalletIcon from "../public/icons/WalletIcon";
-
 import { Button } from "./ui/Button";
 import { SearchBar } from "./SearchBar";
+import { useState } from "react";
+import { useSDK, MetaMaskProvider } from "@metamask/sdk-react";
+import { formatAddress } from "./../lib/utils";
+
+export const ConnectWalletButton = () => {
+  const [account, setAccount] = useState<string | undefined>();
+  const { sdk, connected, connecting } = useSDK();
+
+  const connect = async () => {
+    try {
+      const metaMaskAccount = (await sdk?.connect()) as string;
+      setAccount(metaMaskAccount[0]);
+    } catch (err) {
+      console.warn(`No accounts found`, err);
+    }
+  };
+
+  const disconnect = () => {
+    if (sdk) {
+      sdk.terminate();
+      setAccount(undefined); // Optionally reset the account state
+    }
+  };
+
+  return (
+    <>
+      {connected ? (
+        <div>
+          {formatAddress(account)}{" "}
+          {/* Using the formatAddress utility function */}
+          <button onClick={disconnect}>DISCONNECT</button>{" "}
+          {/* Disconnect button */}
+        </div>
+      ) : (
+        <Button variant="primary" disabled={connecting} onClick={connect}>
+          <WalletIcon className="mr-2 h-4 w-4" /> Connect MetaMask
+        </Button>
+      )}
+    </>
+  );
+};
 
 export const NavBar = () => {
+  const sdkOptions = {
+    logging: { developerMode: false },
+    checkInstallationImmediately: false,
+    dappMetadata: {
+      name: "Demo React App",
+      url: window.location.host,
+    },
+  };
+
   return (
     <nav className="flex mx-auto px-6 py-7 bg-white border rounded-xl items-center justify-between max-w-screen-xl">
-      {/* EventSeaLogo on the left */}
       <Link href="/" className="flex gap-1 px-6">
         <EventSeaLogo />
         <span className="text-2xl font-bold sm:block hidden">
@@ -18,13 +68,11 @@ export const NavBar = () => {
           <span className="text-[#4C6D07]">Sea</span>
         </span>
       </Link>
-
-      {/* Login button with WalletIcon */}
       <div className="flex gap-4 px-6">
         <SearchBar />
-        <Button variant="primary">
-          <WalletIcon className="mr-2 h-4 w-4" /> LogIn
-        </Button>
+        <MetaMaskProvider debug={false} sdkOptions={sdkOptions}>
+          <ConnectWalletButton />
+        </MetaMaskProvider>
       </div>
     </nav>
   );
--- apps/web/lib/utils.ts
+++ apps/web/lib/utils.ts
@@ -1,6 +1,22 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+// utils.ts
+
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const formatBalance = (rawBalance: string) => {
+  const balance = (parseInt(rawBalance) / 1000000000000000000).toFixed(2);
+  return balance;
+};
+
+export const formatChainAsNum = (chainIdHex: string) => {
+  const chainIdNum = parseInt(chainIdHex);
+  return chainIdNum;
+};
+
+export const formatAddress = (addr: string | undefined) => {
+  return `${addr?.substring(0, 8)}...`;
+};
