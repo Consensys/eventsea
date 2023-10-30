f886527023e1efb1a0a970c2e488b1a01c66a222
meowyx <qafka256@gmail.com>
2023-10-31 00:18:00 +0600

    fixed typeerror

--- apps/web/app/events/page.tsx
+++ apps/web/app/events/page.tsx
@@ -2,22 +2,7 @@ import EventDetails from "@/components/EventDetails";
 import { events } from "@/mock-data";
 
 export default function Page() {
-  const event = events[0]; // use the first event as an example
+  const event = events[0];
 
-  const date = new Date(
-    typeof event.dateTime === "number"
-      ? event.dateTime * 1000
-      : event.dateTime.start * 1000
-  ).toLocaleDateString();
-
-  return (
-    <EventDetails
-      title={event.title}
-      tag={event.tags[0]} // assuming you want the first tag
-      date={date}
-      description={event.description}
-      location={`${event.location.name}, ${event.location.address}`}
-      imageSrc={event.images ? event.images[0] : ""}
-    />
-  );
+  return <EventDetails eventId={event.id} />;
 }
