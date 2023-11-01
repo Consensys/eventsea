5f537788784549c6cdfad49f7fe5fd4b77ea6f59
Alejandro <smarketing.cuz@gmail.com>
2023-11-01 10:09:06 -0500

    dynamic svg flow created

--- packages/blockchain/contracts/Ticket.sol
+++ packages/blockchain/contracts/Ticket.sol
@@ -1,16 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import "@openzeppelin/contracts/utils/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "erc721a/contracts/ERC721A.sol";
-import "@openzeppelin/contracts/utils/Pausable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract Ticket is ERC721A, Ownable(tx.origin), Pausable {
+contract Ticket is ERC721URIStorage, Ownable(tx.origin), Pausable {
     uint256 public MAX_SUPPLY;
     uint256 public PRICE = 0.001 ether;
     uint256 public constant MAX_PER_MINT = 5;
     address public eventOwner;
-    uint256 public tokenId = totalSupply();
+    uint256 public tokenId = 0;
     string public _contractBaseURI;
     uint public _ticketPrice;
 
@@ -23,7 +24,7 @@ contract Ticket is ERC721A, Ownable(tx.origin), Pausable {
         string memory contractBaseURI,
         uint256 maxSupply,
         uint ticketPrice
-    ) ERC721A(name, symbol) {
+    ) ERC721(name, symbol) {
         MAX_SUPPLY = maxSupply;
         eventOwner = tx.origin;
         _contractBaseURI = contractBaseURI;
@@ -44,20 +45,19 @@ contract Ticket is ERC721A, Ownable(tx.origin), Pausable {
             "Cannot mint more than 5 tickets at a time"
         );
         require(msg.value >= PRICE * amount, "Not enough ETH sent");
-        require(
-            totalSupply() + amount <= MAX_SUPPLY,
-            "Not enough tickets left"
-        );
+        require(tokenId + amount <= MAX_SUPPLY, "Not enough tickets left");
         require(amount > 0, "Amount must be greater than zero");
         _;
     }
 
     function mint(
-        uint256 amount
+        uint256 amount,
+        string memory _tokenURI
     ) external payable whenNotPaused canMint(amount) {
         for (uint256 i = 1; i < amount; i++) {
             tokenId++;
             _safeMint(msg.sender, tokenId);
+            _setTokenURI(tokenId, _tokenURI);
             lockMetadata(amount);
             emit TicketMinted(msg.sender, tokenId);
         }
@@ -65,8 +65,7 @@ contract Ticket is ERC721A, Ownable(tx.origin), Pausable {
 
     function lockMetadata(uint256 quantity) internal {
         for (uint256 i = quantity; i > 0; i--) {
-            uint256 tid = totalSupply() - i;
-            emit PermanentURI(tokenURI(tid), tid, msg.sender);
+            emit PermanentURI(tokenURI(tokenId), tokenId, msg.sender);
         }
     }
 
@@ -96,9 +95,7 @@ contract Ticket is ERC721A, Ownable(tx.origin), Pausable {
         return tokenURI(_tokenId);
     }
 
-
     function setPrice(uint _price) public onlyOwner {
         _ticketPrice = _price;
     }
-
 }
