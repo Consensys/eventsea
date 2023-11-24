3444777de29efb2be5a08bf48d85891a3f82b499
Alejandro <smarketing.cuz@gmail.com>
2023-11-24 10:10:28 -0500

    Fixing bugs

--- apps/web/components/Navbar.tsx
+++ apps/web/components/Navbar.tsx
@@ -2,14 +2,65 @@
 
 import Link from "next/link";
 import EventSeaLogo from "../public/icons/EventSeaLogo";
+import WalletIcon from "../public/icons/WalletIcon";
+import CreateEvent from "@/components/create-event-form";
+import { Button } from "./ui/Button";
 import { SearchBar } from "./SearchBar";
-import { MetaMaskProvider } from "@metamask/sdk-react";
-import { ConnectionModal } from "./LoginModal";
-import { useState } from "react";
-import { ConnectWalletButton } from "./ConnectWalletButton";
+import { useSDK, MetaMaskProvider } from "@metamask/sdk-react";
+import { formatAddress } from "./../lib/utils";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@/components/ui/popover";
+
+export const ConnectWalletButton = () => {
+  const { sdk, connected, connecting, account } = useSDK();
+
+  const connect = async () => {
+    try {
+      await sdk?.connect();
+    } catch (err) {
+      console.warn(`No accounts found`, err);
+    }
+  };
+
+  const disconnect = () => {
+    sdk?.terminate();
+  };
+
+  return (
+    <div className="relative">
+      {connected ? (
+        <Popover>
+          <PopoverTrigger>
+            <Button variant="primary">{formatAddress(account)}</Button>
+          </PopoverTrigger>
+          <PopoverContent className="right-0 z-10 mt-2 bg-white border rounded-md shadow-lg w-44 top-10">
+            <button className="block w-full py-2 pl-2 pr-4 text-left hover:bg-gray-200">
+              My Events
+            </button>
+            <button className="block w-full py-2 pl-2 pr-4 text-left hover:bg-gray-200">
+              Wallet Details
+            </button>
+            <button
+              onClick={disconnect}
+              className="block w-full pl-2 pr-4 py-2 text-left text-[#F05252] hover:bg-gray-200"
+            >
+              Disconnect
+            </button>
+          </PopoverContent>
+        </Popover>
+      ) : (
+        <Button variant="primary" disabled={connecting} onClick={connect}>
+          <WalletIcon className="w-4 h-4 mr-2" /> Connect MetaMask
+        </Button>
+      )}
+    </div>
+  );
+};
 
 export const NavBar = () => {
-  const [showModal, setShowModal] = useState(false);
   const host =
     typeof window !== "undefined" ? window.location.host : "defaultHost";
 
@@ -36,8 +87,7 @@ export const NavBar = () => {
         <CreateEvent />
 
         <MetaMaskProvider debug={false} sdkOptions={sdkOptions}>
-          <ConnectWalletButton setShowModal={setShowModal} />
-          <ConnectionModal open={showModal} setOpen={setShowModal} />
+          <ConnectWalletButton />
         </MetaMaskProvider>
       </div>
     </nav>
