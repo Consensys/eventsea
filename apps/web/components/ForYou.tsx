dc90616836b3da211319b023231f94b324657fee
meowyx <qafka256@gmail.com>
2023-12-01 00:16:09 +0600

    added events table

--- apps/web/components/ForYou.tsx
+++ apps/web/components/ForYou.tsx
@@ -59,59 +59,6 @@ const ForYou: React.FC<Props> = ({ events }) => {
         </TableBody>
       </Table>
     </>
-    // <div className="p-6">
-    //   <div className="flex justify-between items-center mb-6">
-    //     <h2 className="text-2xl text-gray-900 font-semibold">Events for you</h2>
-    //     <div className="flex items-center">
-    //       <span className="mr-4 text-gray-900">Showing events in</span>
-    //       <select
-    //         value={selectedCountry}
-    //         onChange={(e) => setSelectedCountry(e.target.value)}
-    //         className="border text-gray-900 rounded p-2 bg-white"
-    //       >
-    //         {countries.map((country, index) => (
-    //           <option key={index} value={country}>
-    //             {country}
-    //           </option>
-    //         ))}
-    //       </select>
-    //     </div>
-    //   </div>
-
-    //   <div className="border-t border-gray-200 mt-4 bg-white rounded-lg shadow">
-    //     {events.length > 0 ? (
-    //       events.map((event, index) => (
-    //         <div
-    //           key={index}
-    //           className="flex justify-between items-center py-4 px-4 border-b border-gray-200"
-    //         >
-    //           <div className="flex items-center">
-    //             <img
-    //               src={event.image}
-    //               alt="Event"
-    //               className="w-16 h-16 mr-4 rounded-lg shadow"
-    //             />
-    //             <div>
-    //               <h4 className="text-lg text-gray-900 font-medium">
-    //                 {event.title}
-    //               </h4>
-    //               <p className="text-gray-600">{event.location.address}</p>
-    //             </div>
-    //           </div>
-    //           <p className="text-gray-600">{formatEventDate(event.dateTime)}</p>
-    //           <div className="flex items-center">
-    //             {/* Display participants or a placeholder */}
-    //           </div>
-    //           <span className="text-gray-600 font-bold">
-    //             {/* Display ticket price or a placeholder */}
-    //           </span>
-    //         </div>
-    //       ))
-    //     ) : (
-    //       <p>Loading events...</p> // Placeholder for loading state
-    //     )}
-    //   </div>
-    // </div>
   );
 };
 
