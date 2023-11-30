4ec1fc4acce4a23d8430fc5a5147423fbf952fb5
Alejandro <smarketing.cuz@gmail.com>
2023-11-30 10:58:32 -0500

    Refactor GetTickets component

--- apps/web/components/GetTickets.tsx
+++ apps/web/components/GetTickets.tsx
@@ -5,6 +5,9 @@ import { Button } from "./ui/Button";
 import { getTicketContract } from "@/lib/getTicketContract";
 import { ContractPermission } from "@/types";
 
+import { GasFeeCard } from "./GasFeeCard";
+import MetaMaskProvider from "@/providers/MetamaskProvider";
+
 interface GetTicketsProps {
   ticketPrice: number;
   ticketNFT: string;
@@ -33,7 +36,10 @@ const GetTickets: React.FC<GetTicketsProps> = ({
     });
     try {
       const token = (
-        await nftContract.mint(numberOfTickets, `https://ipfs.io/ipfs/${metadataHash}`)
+        await nftContract.mint(
+          numberOfTickets,
+          `https://ipfs.io/ipfs/${metadataHash}`
+        )
       ).wait();
       console.log(await token, " Minted");
     } catch (e) {
@@ -42,42 +48,48 @@ const GetTickets: React.FC<GetTicketsProps> = ({
   };
 
   return (
-    <div className="w-3/4 p-8 mx-auto bg-white shadow-xl rounded-xl">
-      <div className="flex items-center justify-between mb-4">
-        <span className="text-gray-600">Ticket Price</span>
-        <span>{ticketPrice}ETH</span>
-      </div>
-      <div className="flex items-center justify-between mb-4">
-        <span className="text-gray-600">Number of tickets</span>
-        <div className="flex">
-          <button
-            onClick={handleDecrement}
-            className="px-4 py-1 bg-gray-200 rounded-xl focus:outline-none"
-            disabled={numberOfTickets <= 1}
-          >
-            -
-          </button>
-          <span className="px-4 py-2">{numberOfTickets}</span>
-          <button
-            onClick={handleIncrement}
-            className="px-4 py-1 bg-gray-200 rounded-xl focus:outline-none"
-          >
-            +
-          </button>
+    <MetaMaskProvider>
+      <div className="w-3/4 p-8 mx-auto bg-white shadow-xl rounded-xl">
+        <div className="flex items-center justify-between mb-4">
+          <span className="text-gray-600">Ticket Price</span>
+          <span>{ticketPrice}ETH</span>
         </div>
+        <div className="flex items-center justify-between mb-4">
+          <span className="text-gray-600">Number of tickets</span>
+          <div className="flex">
+            <button
+              onClick={handleDecrement}
+              className="px-4 py-1 bg-gray-200 rounded-xl focus:outline-none"
+              disabled={numberOfTickets <= 1}
+            >
+              -
+            </button>
+            <span className="px-4 py-2">{numberOfTickets}</span>
+            <button
+              onClick={handleIncrement}
+              className="px-4 py-1 bg-gray-200 rounded-xl focus:outline-none"
+            >
+              +
+            </button>
+          </div>
+        </div>
+        <div className="flex items-center justify-between mb-4">
+          <span className="text-gray-600">Total</span>
+          <span>{(numberOfTickets * ticketPrice).toFixed(2)}ETH</span>
+        </div>
+        <div className="flex items-center justify-between mb-4">
+          <span className="text-gray-600">Gas fee</span>
+          <GasFeeCard />
+        </div>
+        <Button
+          variant="primary"
+          className="w-full text-[#0C200A] p-2 rounded focus:outline-none "
+          onClick={async () => await handleGetTickets()}
+        >
+          Get tickets
+        </Button>
       </div>
-      <div className="flex items-center justify-between mb-4">
-        <span className="text-gray-600">Total</span>
-        <span>{(numberOfTickets * ticketPrice).toFixed(2)}ETH</span>
-      </div>
-      <Button
-        variant="primary"
-        className="w-full text-[#0C200A] p-2 rounded focus:outline-none "
-        onClick={async () => await handleGetTickets()}
-      >
-        Get tickets
-      </Button>
-    </div>
+    </MetaMaskProvider>
   );
 };
 
