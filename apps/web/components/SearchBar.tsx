435109e2c783685e2ca1414f4e16d5b33155b0c5
Alejandro <smarketing.cuz@gmail.com>
2023-12-14 12:37:12 -0500

    Refactor search bar component

--- apps/web/components/SearchBar.tsx
+++ apps/web/components/SearchBar.tsx
@@ -2,40 +2,25 @@
 
 import SearchIcon from "../public/icons/SearchIcon";
 import debounce from "lodash.debounce";
-import { FC, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import {
+  Command,
   CommandDialog,
   CommandEmpty,
   CommandGroup,
   CommandInput,
   CommandItem,
   CommandList,
+  CommandSeparator,
 } from "@/components/ui/command";
 import { getEvents } from "@/lib/actions";
 import { EventSea } from "@/types";
 import Link from "next/link";
 import { Button } from "./ui/Button";
 
-interface SearchInputProps {
-  searchValue: string;
-  setSearchValue: (value: string) => void;
-}
-
-const SearchInput: FC<SearchInputProps> = ({ searchValue, setSearchValue }) => {
-  return (
-    <CommandInput
-      value={searchValue}
-      onValueChange={setSearchValue}
-      className="combobox-input"
-      placeholder="Search an event...."
-    />
-  );
-};
-
 export const SearchBar = () => {
   const [events, setEvents] = useState<EventSea.Event[]>([]);
   const [searchValue, setSearchValue] = useState("");
-  const [loading, setIsLoading] = useState(false);
 
   const [open, setOpen] = useState(false);
 
@@ -44,11 +29,8 @@ export const SearchBar = () => {
       if (!searchValue) {
         return setEvents([]);
       }
-      setIsLoading(true);
       const events = await getEvents(searchValue);
-      console.log(events?.map((event) => event.title));
       setEvents(events || []);
-      setIsLoading(false);
     };
 
     fetchEvents();
@@ -71,64 +53,66 @@ export const SearchBar = () => {
   }, [searchValue]);
 
   return (
-    <div className="flex items-center gap-4">
-      <div className="">
-        <Button
-          onClick={() => setOpen(true)}
-          variant="outline"
-          className="px-2 bg-muted md:px-4"
-        >
-          <SearchIcon className="w-5 h-5 sm:hidden" />
-          <div className="justify-between hidden w-36 md:w-48 sm:flex">
-            search events
-            <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-card-foreground opacity-100">
-              <span className="text-xs">⌘</span>K
-            </kbd>
-          </div>
-        </Button>
-      </div>
-      <CommandDialog
-        open={open}
-        onOpenChange={() => {
-          setOpen(!open);
-          setSearchValue("");
-          setEvents([]);
-        }}
+    <>
+      <Button
+        onClick={() => setOpen(true)}
+        variant="outline"
+        className="px-2 bg-muted md:px-4"
       >
-        <SearchInput
-          searchValue={searchValue}
-          setSearchValue={(val) => setSearchValue(val)}
-        />
-        <CommandList>
-          <CommandEmpty className="py-0">
-            {loading ? (
-              <div className="w-full h-1 rounded bg-primary animate-pulse"></div>
-            ) : (
-              <div className="py-6 text-center">No events found</div>
-            )}
-          </CommandEmpty>
-          <CommandList>
-            <CommandGroup>
-              {!loading &&
-                events.map(({ id, title }) => (
-                  <CommandItem
-                    key={id}
-                    value={title}
-                    onSelect={() => {
-                      setOpen(false);
-                      setSearchValue("");
-                      setEvents([]);
-                    }}
-                  >
-                    <Link className="flex-1" href={`/events/${id}`}>
-                      {title}
-                    </Link>
-                  </CommandItem>
-                ))}
-            </CommandGroup>
-          </CommandList>
-        </CommandList>
-      </CommandDialog>
-    </div>
+        <SearchIcon className="w-5 h-5 sm:hidden" />
+        <div className="justify-between hidden w-36 md:w-48 sm:flex">
+          <span className="text-gray-500">Find your next event...</span>
+          <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-card-foreground opacity-100">
+            <span className="text-xs">⌘</span>K
+          </kbd>
+        </div>
+      </Button>
+      {open && (
+        <CommandDialog
+          open={open}
+          onOpenChange={() => {
+            setOpen(!open);
+            setSearchValue("");
+            setEvents([]);
+          }}
+        >
+          <Command className="rounded-lg border shadow-md">
+            <CommandInput
+              value={searchValue}
+              onValueChange={setSearchValue}
+              placeholder="Find your next event..."
+            />
+            <CommandList>
+              <CommandEmpty>No results found.</CommandEmpty>
+              <CommandList>
+                <CommandList>
+                  <CommandGroup>
+                    {events.length > 0 && <CommandSeparator />}
+                    {events.length > 0 &&
+                      events.map(({ id, title }) => (
+                        <>
+                          <CommandItem
+                            key={id}
+                            value={title}
+                            onSelect={() => {
+                              setOpen(false);
+                              setSearchValue("");
+                              setEvents([]);
+                            }}
+                          >
+                            <Link className="flex-1" href={`/events/${id}`}>
+                              {title}
+                            </Link>
+                          </CommandItem>
+                        </>
+                      ))}
+                  </CommandGroup>
+                </CommandList>
+              </CommandList>
+            </CommandList>
+          </Command>
+        </CommandDialog>
+      )}
+    </>
   );
 };
