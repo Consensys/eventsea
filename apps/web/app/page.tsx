d8b413bbd98a3beaf699c198e07453df0766c455
Alejandro <smarketing.cuz@gmail.com>
2023-12-14 14:03:39 -0500

    Refactor events section in Page component

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -11,12 +11,14 @@ export default async function Page(): Promise<JSX.Element> {
   return (
     <main className="space-y-6 md:px-10">
       <FeaturedEvents events={events.reverse().slice(0, 8)} />
-      <section>
-        <h2 className="text-[#09090B] font-semibold text-[28px] mb-4">
-          Events for you
-        </h2>
-        {events.length > 0 && <ForYou events={events} />}
-      </section>
+      {events.length > 0 && (
+        <section>
+          <h2 className="text-[#09090B] font-semibold text-[28px] mb-4">
+            Events for you
+          </h2>
+          <ForYou events={events} />
+        </section>
+      )}
     </main>
   );
 }
