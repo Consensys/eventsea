a5ba89b61fb070e7d4e47c07ef6952388b4c8edb
Alejandro <smarketing.cuz@gmail.com>
2024-02-23 17:14:51 -0500

    Update environment variables and configuration files

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,7 +3,6 @@ module.exports = {
   env: {
     INFURA_API_KEY: process.env.INFURA_API_KEY,
     INFURA_API_SECRET: process.env.INFURA_API_SECRET,
-    INFURA_IPFS_ENDPOINT: process.env.INFURA_IPFS_ENDPOINT,
   },
   transpilePackages: ["ui"],
   images: {
